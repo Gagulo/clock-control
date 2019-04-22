9c4d6079a2f043055ae85812b4d5717d09c97b02
Gagulo <grzegorz.sokalski@hotmail.com>
2019-04-22 17:16:11 +0200

    closer to goal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,28 @@ export class AppComponent  {
   input2: number;
   errorMessage: string;
   presentDate: Date;
+  userPick: Date;
 
   constructor() {
     this.presentDate = new Date();
     this.input1 = this.presentDate.getHours();
     this.input2 = this.presentDate.getMinutes();
+    this.userPick = new Date();
   }
   testTime(){
     console.log(`Hour = ${this.input1}, and Minutes = ${this.input2}` );
 
-    const userPick = new Date(); 
     this.errorMessage = ''
 
-    if ( userPick > this.presentDate ) {
+    if ( this.userPick > this.presentDate ) {
+      this.input1 = this.userPick.getHours();
+      this.input2 = this.userPick.getMinutes();
       this.errorMessage = 'This is a future time dude!'
     }
-    if (userPick.getHours() > this.input1){
+    if ( this.userPick.getHours() > this.input1 ){
       this.errorMessage = "Wrong Hours dude!"
     }
-    if (userPick.getMinutes() > this.input2){
+    if ( this.userPick.getMinutes() > this.input2 ){
       this.errorMessage = "Wrong Minutes dude!"
     }
   }
